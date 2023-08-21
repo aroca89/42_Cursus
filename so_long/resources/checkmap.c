36e24044982d6602eafbdf2014211101b6f00c1d
aroca89 <arocapazos@gmail.com>
2023-08-21 20:28:24 +0200

    makefile funcionando , creado y funcionando make de gnl, comprobado checkargument.c

--- so_long/resources/checkmap.c
+++ so_long/resources/checkmap.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   checkmap.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aroca-pa <aroca-pa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 11:24:52 by aroca-pa          #+#    #+#             */
-/*   Updated: 2023/08/21 19:51:40 by aroca-pa         ###   ########.fr       */
+/*   Created: 2023/08/21 20:26:13 by aroca-pa          #+#    #+#             */
+/*   Updated: 2023/08/21 20:28:23 by aroca-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H 
-
-# include <stdio.h>
-
-void check_argument(int argc, char *argv[]);
-int main(int argc, char *argv[]);
-
-
-#endif
--- so_long/resources/main.c
+++ so_long/resources/main.c
@@ -6,15 +6,16 @@
 /*   By: aroca-pa <aroca-pa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:24:48 by aroca-pa          #+#    #+#             */
-/*   Updated: 2023/08/11 12:24:28 by aroca-pa         ###   ########.fr       */
+/*   Updated: 2023/08/21 20:28:23 by aroca-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"so_long.h"
+#include "../so_long.h"
 
 int main(int argc, char *argv[])
 {
     check_argument(argc, argv);
+    check_map();
     
     return 0;
 }
