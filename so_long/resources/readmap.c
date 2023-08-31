08a6fd731b192af856bef16b9cb7af96fc3901d7
aroca89 <arocapazos@gmail.com>
2023-08-31 17:56:28 +0200

    añadidos comentarios de flujo readmap

--- so_long/resources/readmap.c
+++ so_long/resources/readmap.c
@@ -6,7 +6,7 @@
 /*   By: aroca-pa <aroca-pa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 20:04:10 by aroca-pa          #+#    #+#             */
-/*   Updated: 2023/08/30 20:37:22 by aroca-pa         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:55:36 by aroca-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,54 +23,78 @@ static int len_lines(const char *filename)
     int num_lines = 0;
     int fd = open(filename, O_RDONLY);
 
+    // Verificar si se pudo abrir el archivo
     if (fd == -1)
     {
         perror("Error al abrir el archivo\n");
         exit(EXIT_FAILURE);
     } 
 
+    // Contar el número de líneas en el archivo usando 'get_next_line'
     while (get_next_line(fd))
         num_lines++;
 
+    // Cerrar el archivo después de haber contado las líneas
     close(fd);
+
+    // Devolver el número total de líneas en el archivo
     return num_lines;
 }
 
+
 static char **read_lines(const char *filename, int num_lines)
 {
+    // Reservar memoria para un arreglo de punteros a caracteres (líneas del mapa)
     char **map = (char **)malloc((num_lines + 1) * sizeof(char *));
     int fd = open(filename, O_RDONLY);
 
+    // Verificar si se asignó memoria correctamente para 'map'
     if (!map)
         ft_lst_perror(MALLOC_ERROR, NULL);
 
+    // Verificar si se pudo abrir el archivo
     if (fd == -1)
         ft_lst_perror(INVALID_FILE, NULL);
 
     char *line;
     int i = 0;
+    // Leer cada línea del archivo usando la función 'get_next_line'
     while ((line = get_next_line(fd)) != NULL)
     {
+        // Asignar la línea leída al elemento 'i' del arreglo bidimensional 'map'
         map[i] = line;
         i++;
     }
-    map[i] = NULL; // Marca el final de la matriz con NULL
+    map[i] = NULL; // Marcar el final del arreglo con un puntero NULL
 
+    // Cerrar el archivo después de haber leído todas las líneas
     close(fd);
+
+    // Devolver el arreglo bidimensional 'map' que contiene las líneas del archivo
     return map;
 }
+
 
 t_map *convert_maps(const char *filename)
 {
+    // Obtener el número de líneas en el archivo
     int num_lines = len_lines(filename);
+    
+    // Leer las líneas del archivo y almacenarlas en un arreglo bidimensional
     char **map_data = read_lines(filename, num_lines);
     
+    // Reservar memoria para una estructura de tipo t_map y asignarla a 'map'
     t_map *map = (t_map *)ft_calloc(1, sizeof(t_map));
     if (!map)
         ft_lst_perror(MALLOC_ERROR, map);
-    map->data = map_data; // Asignar el arreglo bidimensional a la estructura
+    
+    // Asignar el arreglo bidimensional 'map_data' al campo 'data' de la estructura 'map'
+    map->data = map_data;
+    
+    // Devolver el puntero a la estructura t_map que contiene los datos del mapa
     return map;
 }
+
 
 t_map *copy_map(t_map *map)
 {
