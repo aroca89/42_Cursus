03418f400d0486ce7417054264869f59220a6b5a
aroca89 <arocapazos@gmail.com>
2023-08-31 17:59:47 +0200

    añadidos comentarios errorlist

--- so_long/resources/errorlist.c
+++ so_long/resources/errorlist.c
@@ -6,7 +6,7 @@
 /*   By: aroca-pa <aroca-pa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 17:22:36 by aroca-pa          #+#    #+#             */
-/*   Updated: 2023/08/28 17:40:22 by aroca-pa         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:58:58 by aroca-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,29 @@
 
 void ft_lst_perror(int type, t_map *map)
 {
+    // Comprobar el tipo de error y mostrar el mensaje correspondiente
     if (type == 1)
-		perror("Wrong number of arguments.\n");
-	else if (type == INVALID_FILE)
-		perror("Error\nNo such file or directory\n");
-	else if (type == INVALID_MAP)
-		perror("Error\nInvalid map.Make sure the map is rectangle and is formed only by: '0', '1', 'E', 'P' and 'C'\n");
-	else if (type == INVALID_EXTENSION)
-		perror("Error.\nInvalid file extension. Make sure it is '.ber'.\n");
-	else if (type == MALLOC_ERROR)
-		perror("Error.\nFailed to allocate memory.\n");
-	else if (type == EMPTY_MAP)
-		perror("Error.\nThe file is empty.\n");
-	else if (type == PLAYER_ERROR)
-		perror("Error.\nThere are two or more player positions.\n");
-	else if (type == WALL_ERROR)
-		perror("Error\nThe map is no closed by '1'.\n");
-	else if (type == IMAGE_ERROR)
-		perror("Error.\nThe image is not xpm.\n");
-	free_map(map);
-	exit(-1);
+        perror("Wrong number of arguments.\n");
+    else if (type == INVALID_FILE)
+        perror("Error\nNo such file or directory\n");
+    else if (type == INVALID_MAP)
+        perror("Error\nInvalid map.Make sure the map is rectangle and is formed only by: '0', '1', 'E', 'P' and 'C'\n");
+    else if (type == INVALID_EXTENSION)
+        perror("Error.\nInvalid file extension. Make sure it is '.ber'.\n");
+    else if (type == MALLOC_ERROR)
+        perror("Error.\nFailed to allocate memory.\n");
+    else if (type == EMPTY_MAP)
+        perror("Error.\nThe file is empty.\n");
+    else if (type == PLAYER_ERROR)
+        perror("Error.\nThere are two or more player positions.\n");
+    else if (type == WALL_ERROR)
+        perror("Error\nThe map is not closed by '1'.\n");
+    else if (type == IMAGE_ERROR)
+        perror("Error.\nThe image is not xpm.\n");
+
+    // Liberar la memoria asignada al mapa
+    free_map(map);
+
+    // Salir del programa con un código de error
+    exit(-1);
 }
